f9542bdf73ff0435430681c140ba811ba0fe05bf
Luis Sierra <rsierramancebo@gmail.com>
2020-09-08 18:07:45 -0400

    Make puestos the home page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::redirect('/', '/puestos');
 
 Route::resources([
     'candidatos' => 'CandidatosController',

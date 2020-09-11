86e2274588d021978c116c9bbbf83ddebf928d48
Luis Sierra <rsierramancebo@gmail.com>
2020-09-11 17:30:02 -0400

    Missed job position form

--- resources/views/puestos/create.blade.php
+++ resources/views/puestos/create.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Crear Puesto')
 
 @section('content')
+    <div class="row mt-2 mb-4">
+        <div class="col-12">
+            <h2>Crear Puesto</h2>
+        </div>
+    </div>
+
     <form method="POST" action="{{route('puestos.store')}}">
         @csrf
         @include('includes.errors')
@@ -10,22 +16,32 @@
             <label for="nombre">Nombre</label>
             <input id="nombre" type="text" name="nombre" class="@error('nombre') is-invalid @enderror form-control">
         </div>
-        <div class="form-group">
-            <label for="nivel_riesgo">Nivel de Riesgo</label>
-            <select id="nivel_riesgo" name="nivel_riesgo" class="@error('nivel_riesgo') is-invalid @enderror custom-select">
-                <option value="alto">alto</option>
-                <option value="medio">medio</option>
-                <option value="bajo">bajo</option>
-            </select>
+
+        <div class="form-row">
+            <div class="col-4">
+                <div class="form-group">
+                    <label for="min_salary">Salario Minimo</label>
+                    <input id="min_salary" type="number" name="min_salary" class="@error('min_salary') is-invalid @enderror form-control">
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="form-group">
+                    <label for="max_salary">Salario Maximo</label>
+                    <input id="max_salary" type="number" name="max_salary" class="@error('max_salary') is-invalid @enderror form-control">
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="form-group">
+                    <label for="nivel_riesgo">Nivel de Riesgo</label>
+                    <select id="nivel_riesgo" name="nivel_riesgo" class="@error('nivel_riesgo') is-invalid @enderror custom-select">
+                        <option value="alto">alto</option>
+                        <option value="medio">medio</option>
+                        <option value="bajo">bajo</option>
+                    </select>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="min_salary">Salario Minimo</label>
-            <input id="min_salary" type="number" name="min_salary" class="@error('min_salary') is-invalid @enderror form-control">
-        </div>
-        <div class="form-group">
-            <label for="max_salary">Salario Maximo</label>
-            <input id="max_salary" type="number" name="max_salary" class="@error('max_salary') is-invalid @enderror form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">Crear</button>
+
+        <button type="submit" class="btn btn-primary btn-lg">Crear</button>
     </form>
 @endsection
--- resources/views/puestos/update.blade.php
+++ resources/views/puestos/update.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.app')
 
-@section('title', 'Editar Puesto')
+@section('title', 'Actualizar Puesto')
 
 @section('content')
+    <div class="row mt-2 mb-4">
+        <div class="col-12">
+            <h2>Actualizar Puesto</h2>
+        </div>
+    </div>
+
     <form method="POST" action="{{route('puestos.update', $puesto)}}">
         @method('PUT')
         @csrf
@@ -11,29 +17,41 @@
             <label for="nombre">Nombre</label>
             <input id="nombre" type="text" name="nombre" class="@error('nombre') is-invalid @enderror form-control"  value="{{$puesto->nombre}}">
         </div>
-        <div class="form-group">
-            <label for="nivel_riesgo">Nivel de Riesgo</label>
-            <select id="nivel_riesgo" name="nivel_riesgo" class="@error('nivel_riesgo') is-invalid @enderror custom-select">
-                <option value="alto" @if($puesto->estado == 'alto') selected @endif>alto</option>
-                <option value="medio" @if($puesto->estado == 'medio') selected @endif>medio</option>
-                <option value="bajo" @if($puesto->estado == 'bajo') selected @endif>bajo</option>
-            </select>
+
+        <div class="form-row">
+            <div class="col-3">
+                <div class="form-group">
+                    <label for="nivel_riesgo">Nivel de Riesgo</label>
+                    <select id="nivel_riesgo" name="nivel_riesgo" class="@error('nivel_riesgo') is-invalid @enderror custom-select">
+                        <option value="alto" @if($puesto->estado == 'alto') selected @endif>alto</option>
+                        <option value="medio" @if($puesto->estado == 'medio') selected @endif>medio</option>
+                        <option value="bajo" @if($puesto->estado == 'bajo') selected @endif>bajo</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <label for="min_salary">Salario Minimo</label>
+                    <input id="min_salary" type="number" name="min_salary" class="@error('min_salary') is-invalid @enderror form-control" value="{{$puesto->salario_minimo}}">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <label for="max_salary">Salario Maximo</label>
+                    <input id="max_salary" type="number" name="max_salary" class="@error('max_salary') is-invalid @enderror form-control" value="{{$puesto->salario_maximo}}">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <label for="estado">Estado</label>
+                    <select id="estado" name="estado" class="@error('estado') is-invalid @enderror custom-select">
+                        <option value="inactivo" @if($puesto->estado == 'inactivo') selected @endif>inactivo</option>
+                        <option value="activo" @if($puesto->estado == 'activo') selected @endif>activo</option>
+                    </select>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="min_salary">Salario Minimo</label>
-            <input id="min_salary" type="number" name="min_salary" class="@error('min_salary') is-invalid @enderror form-control" value="{{$puesto->salario_minimo}}">
-        </div>
-        <div class="form-group">
-            <label for="max_salary">Salario Maximo</label>
-            <input id="max_salary" type="number" name="max_salary" class="@error('max_salary') is-invalid @enderror form-control" value="{{$puesto->salario_maximo}}">
-        </div>
-        <div class="form-group">
-            <label for="estado">Estado</label>
-            <select id="estado" name="estado" class="@error('estado') is-invalid @enderror custom-select">
-                <option value="inactivo" @if($puesto->estado == 'inactivo') selected @endif>inactivo</option>
-                <option value="activo" @if($puesto->estado == 'activo') selected @endif>activo</option>
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">Actualizar</button>
+
+        <button type="submit" class="btn btn-primary btn-lg">Actualizar</button>
     </form>
 @endsection
